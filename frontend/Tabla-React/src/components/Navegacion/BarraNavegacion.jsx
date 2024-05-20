30c2d8e6531e3f22ec97c637fe0801c5274321ca
Sebastian <sebabutcovich@gmail.com>
2024-05-20 13:05:34 -0300

    Barra de navegación incompleta

--- frontend/Tabla-React/src/components/Navegacion/BarraNavegacion.jsx
+++ frontend/Tabla-React/src/components/Navegacion/BarraNavegacion.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './Navegacion.css'
+import  imgLogin from '../../recursos/imgNavBar/usuario2.png'
 function BarraNavegacion() {
   return (
     <div className='Barra-Navegacion'>
         <div className='Barra-Usuario'>
-            <div className="Barra-Navegacion-Links">
+          <div className="contenedor-titulo-navBar">
+            <h1 className="titulo-navBar">Aplicacion Finanzas</h1>
+          </div>
+            <div className="Barra_Navegacion_Links">
               <a href="" className="">Gastos</a>
               <a href="" className="">Ingresos</a>
+              < a href="" className="">Sobre nosotros</a>
               <a href="" className=""></a>
             </div>
-            <icon className="">Icono</icon>
+            <div className="contenedor-usuario">
+              <img className='logo_usuario' src={imgLogin}alt=""/>
+              <span>Nombre del usuario</span>
+            </div>
         </div>
     </div>
   )
