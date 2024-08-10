65c8dc3d308b569635ac1184a3a1f7eb0669cbb9
bdgarat <garat.braian@gmail.com>
2024-08-10 13:56:15 -0300

    bugfix en funcion conversion a moneda extranjera

--- backend/app/utils/convert_to_foreign_currency.py
+++ backend/app/utils/convert_to_foreign_currency.py
@@ -7,7 +7,9 @@ def convert_to_foreign_currency(value_pesos: float, currency: str = "dol", type_
     else:
         response = requests.get(f"https://dolarapi.com/v1/cotizaciones/{currency}")
     if response.status_code == 200:
-        currency_venta = response.json().get("venta", 0.0)
+        currency_venta = response.json().get("venta", 1.0)
         return value_pesos / currency_venta
     else:
         raise Exception(f"Error en llamada a API de cotizaciones. Status_code: {response.status_code}, Reason: {response.reason}, text: {response.text}")
+
+# Documentacion microservicio externo: https://dolarapi.com/docs/argentina/
