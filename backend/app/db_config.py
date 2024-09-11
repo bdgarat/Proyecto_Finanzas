84ba891c7da2555ec2805379fddbd8647dc8031a
Sebastian Butocvich <sebabutcovich@gmail.com>
2024-09-11 18:35:18 -0300

    Prueba 06

--- backend/app/db_config.py
+++ backend/app/db_config.py
@@ -1,6 +1,6 @@
 db_config = {
     "USER": "root",
     "PASSWORD": "wEFTgLPxAgdzTzXKQrAOPdNTriaExmEg",
-    "HOST": "mysql://root:wEFTgLPxAgdzTzXKQrAOPdNTriaExmEg@mysql.railway.internal:3306/railway",
+    "HOST": "mysql://root:wEFTgLPxAgdzTzXKQrAOPdNTriaExmEg@autorack.proxy.rlwy.net:49886/proyecto_finanzas_develop",
     "DATABASE": "proyecto_finanzas_develop"
 }
