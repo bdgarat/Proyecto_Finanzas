937e1e9772036f74f93dceebf11196ddc508c97b
Sebastian <sebabutcovich@gmail.com>
2024-12-23 12:55:51 -0300

    Token y refresh token

--- Backend_Java_Finanzas_App/app/src/test/java/com/example/tryJwt/demo/DemoApplicationTests.java
+++ Backend_Java_Finanzas_App/app/src/test/java/com/example/tryJwt/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.back.finanzas.app;
+package com.example.tryJwt.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
